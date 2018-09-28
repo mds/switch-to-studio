7a9850fdc03efbff00b481d4a342254c6d053274
Matt D Smith <mds@mds.local>
2018-09-28 14:51:02 -0400

    initial commit

---
+++ src/_scripts/index.js
@@ -0,0 +1,10 @@
+// @codekit-prepend 'vendor/jquery.js'
+
+$(document).ready(function() {
+
+  // console.log("Look ma, no errors!\n\nBuuuuuut.... if you see something crazy or just plain wrong though, please reach out. I only half know what I'm doing here. A DM or public reply is perfectly acceptable.\n\n🖤\n—MDS\nhttp://twitter.com/mds\n\n");
+  console.log("Thanks for popping open the inspector.\nIf you see something wrong, please let me know!\nA DM or public reply is perfectly acceptable.\n\n—MDS\nhttp://twitter.com/mds\n\n");
+
+
+});
+
